4fdea371a89d021a07f23d5418d1fa281679406c
Gelanor <91592252+VladimirGelanorGrachev@users.noreply.github.com>
2022-04-09 15:47:03 +0300

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _hp;
+        [SerializeField] private Rigidbody2D _bullet;
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private float _force;
+        private Camera _camera;
+        private Ship _ship;
+        private void Start()
+        {
+            _camera = Camera.main;
+            var moveTransform = new AccelerationMove(transform, _speed,
+            _acceleration);
+            var rotation = new RotationShip(transform);
+            _ship = new Ship(moveTransform, rotation);
+        }
+        private void Update()
+        {
+            var direction = Input.mousePosition -
+            _camera.WorldToScreenPoint(transform.position);
+            _ship.Rotation(direction);
+            _ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),
+            Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _ship.AddAcceleration();
+            }
+        }
+    }
+}
